a781f875ca77ff3f59207c05deebe12d077686cb
goju2k <goju2k@naver.com>
2021-08-30 00:13:51 +0900

    image limit size changed to 4k -> 5m in url-loader

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,13 @@ module.exports = {
     : '/',
     lintOnSave:false,
     productionSourceMap:!production,
+    chainWebpack: config => {
+
+        config.module.rule('images').use('url-loader')
+        .tap(options => {
+            options.limit = 5000000 //이미지 사이즈 5메가 이하는 전부 js안으로 빌드됨
+            return options;
+        });
+
+    },
 }
