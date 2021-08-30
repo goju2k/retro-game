b22d80c4f30c5f9be3b5517a1378fa10a362eaea
goju2k <goju2k@naver.com>
2021-08-30 22:35:33 +0900

    remove vue global property

--- src/main.js
+++ src/main.js
@@ -2,27 +2,8 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 
-import math from '@/modules/util/math.js'
-import num from '@/modules/const/num.js'
-
-import device from '@/game/config/device.js'
-
-import globalVar from "@/modules/base/GlobalVar";
-
 //앱 생성
 const app = createApp(App);
-
-//글로벌 객체 로딩
-app.config.globalProperties.$math = math;
-app.config.globalProperties.$num = num;
-
-app.config.globalProperties.$res = {};
-
-//device config 로딩
-app.config.globalProperties.$device = device;
-
-//class global 변수에 vue global 셋팅
-globalVar.$vue = app.config.globalProperties;
 
 //앱 실행
 app.mount('#app')
