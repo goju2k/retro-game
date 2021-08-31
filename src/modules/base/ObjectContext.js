e01fce230bd1066f943c1910c020764e970362ab
goju2k <goju2k@naver.com>
2021-08-31 14:28:52 +0900

    change scope of global vars

--- src/modules/base/ObjectContext.js
+++ src/modules/base/ObjectContext.js
@@ -1,27 +1,31 @@
+let mapInfo = {
+    mapData: null,
+    mapSection: null,
+};
+
 class ObjectContext{
 
     constructor(){
-        this.mapData;
-        this.mapSection;
+        this.mapInfo = mapInfo;
     }
 
     //맵 정보 초기화
     $setupMapInfo = function(map, mapSprite){
 
-        this.mapData = map;
+        this.mapInfo.mapData = map;
 
         const xcnt = map.width / map.blockSize;
         const ycnt = map.height / map.blockSize;
 
-        this.mapSection = [];
+        this.mapInfo.mapSection = [];
 
         for(let i = 0 ; i < xcnt ; i++){
 
-            this.mapSection[i] = [];
+            this.mapInfo.mapSection[i] = [];
 
             for(let k = 0 ; k < ycnt ; k++){
 
-                this.mapSection[i][k] = Object.assign({
+                this.mapInfo.mapSection[i][k] = Object.assign({
 
                     sprite : mapSprite, //맵 스프라이트
                     
@@ -40,7 +44,10 @@ class ObjectContext{
 
     //맵 정보 get
     $getMap = function(){
-        return this.mapSection;
+        return this.mapInfo.mapSection;
+    }
+    $getMapData = function(){
+        return this.mapInfo.mapData;
     }
     
 };
