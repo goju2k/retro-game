112685c8fc5222dd74bc374753d9db7079993e60
goju2k <goju2k@naver.com>
2021-08-31 14:28:13 +0900

    applied roaming behavior

--- src/game/object/monster/turtle/Turtle.js
+++ src/game/object/monster/turtle/Turtle.js
@@ -21,6 +21,22 @@ class Turtle extends AbstractObject{
         this.visibleRadius = 100;
         this.updateVisible(); //시야 위치 업데이트
 
+        //충돌박스 offset 설정
+        this.colliderOffset = {
+            boxList:[
+                [6, 1, 22, 30]
+            ],
+        }
+
+        //충돌박스 업데이트
+        this.updateCollider();
+
+        //상태
+        this.status = 0;
+
+        console.log("this.$getMapData()", this.$getMapData());
+        console.log("this.$getMap()", this.$getMap());
+        
     }
 
     //계산
@@ -39,11 +55,15 @@ class Turtle extends AbstractObject{
 
             //시야 위치 업데이트
             this.updateVisible()
+            
+            //충돌박스 업데이트
+            this.updateCollider();
 
             //이동 애니메이션 결정
             if(this.currAction.status == 0){
                 this.animation.pose.setAnimation('pose');
                 this.action = 0;
+                this.status = 0;
             }else{
                 this.currAction.moveDirectionH==1?this.animation.pose.setAnimation('run_right'):this.animation.pose.setAnimation('run_left');
             }
@@ -59,8 +79,8 @@ class Turtle extends AbstractObject{
 
         //시야 반경 그리기
         ctx.beginPath();
-        ctx.strokeStyle = this.action == 1?'red':'lightgreen';
-        ctx.arc(this.visibleX, this.visibleY, this.visibleRadius, 0, 2*Math.PI);
+        ctx.strokeStyle = this.status == 2?'red':'lightgreen';
+        ctx.arc(this.visibleX, this.visibleY, this.visibleRadius, 0, this.$math.PI2);
         ctx.stroke();
         
     }
@@ -75,27 +95,36 @@ class Turtle extends AbstractObject{
         //생각 인터벌 1초 + 알파
         if(this.thinkCurrTime > this.thinkTime){
             this.thinkCurrTime = 0;
-            this.thinkTime = 1000;// + math.random(1000);
+            this.thinkTime = 500;// + math.random(1000);
         }else{
             return;
         }
 
         //적찾기
-        for(let box of this.$g.player.collider.boxList){
-
+        for (let box of this.$g.player.collider.boxList) {
+            
             if(this.$math.checkCrossBoxAndCircle(
                 box[0], box[1], box[2], box[3],
                 this.visibleX, this.visibleY, this.visibleRadius
             )){
                 this.setTarget(this.$g.player.x, this.$g.player.y);
+                this.status = 2; //적발견
                 break;
             }
 
         }
 
+        if (this.status != 2 && this.action == 0) {
+            this.status = 1;
+            this.setTarget(
+                this.$math.random(this.$getMapData().width),
+                this.$math.random(this.$getMapData().height),
+            );
+        }
+
     }
 
-    //공격 목표설정
+    //이동 목표설정
     setTarget(x, y){
 
         if(this.atkTarget[0] == x && this.atkTarget[1] == y){
