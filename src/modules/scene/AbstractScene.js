3ed51c114fbdf7d95dec5ff2d864091665ad2e70
goju2k <goju2k@naver.com>
2021-09-01 17:31:00 +0900

    use OffCanvas

--- src/modules/scene/AbstractScene.js
+++ src/modules/scene/AbstractScene.js
@@ -13,9 +13,49 @@ class AbstractScene extends BaseClass{
         //이름
         this.name = this.status.name || 'unknown scene'
 
+        //캐시용 off캔버스 (배경 등등..)
+        this.offctx = {};
+
+        //offcan name for map
+        this.offCtxMapName = 'map';
+
         //초기화
         this.init(this.status);
 
+    }
+
+    createOffCanvas(name, width, height){
+
+        if(this.offctx[name]){
+            return;
+        }
+
+        const can = document.createElement('canvas');
+        can.width = width;
+        can.height = height;
+        can.style.zIndex = '1'
+        can.style.position = 'absolute';
+        can.style.border = '1px solid black'
+        
+        
+        const ctx = can.getContext('2d');
+        //ctx.scale(this.$g.scaleX, this.$g.scaleY);
+            
+        //image scale option
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
+
+        this.offctx[name] = {
+            can:can,
+            ctx:ctx,
+        }
+
+    }
+
+    getOffCanvas(name){
+        return this.offctx[name];
     }
 
     //초기화
@@ -33,10 +73,15 @@ class AbstractScene extends BaseClass{
         //this.log('draw');
     }
 
-    //맵 그리기
-    drawMap(ctx, gapTime, keyInput){
+    //맵 초기화
+    initMap(){
 
+        this.createOffCanvas(this.offCtxMapName, this.$getMapData().width, this.$getMapData().height);
+        
         const mapSection = this.$getMap();
+        const ctx = this.getOffCanvas(this.offCtxMapName).ctx;
+
+        ctx.clearRect(0, 0, this.$getMapData().width, this.$getMapData().height);
 
         for(let mapRow of mapSection){
 
@@ -50,6 +95,13 @@ class AbstractScene extends BaseClass{
             }
 
         }
+
+    }
+
+    //맵 그리기
+    drawMap(ctx, gapTime, keyInput){
+
+        ctx.drawImage(this.getOffCanvas(this.offCtxMapName).can, 0, 0);
 
     }
 
